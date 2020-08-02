02c5e9b2fd1a75267f71a40ebb209111acb1a600
Daniel Kreuzhofer <1763467+kreuzhofer@users.noreply.github.com>
2020-08-02 07:15:59 +0200

    make editing builddefinitions work

--- src/BuildDefinition.js
+++ src/BuildDefinition.js
@@ -7,6 +7,8 @@ import {
     List, 
     Segment, 
     Button,
+    TextArea,
+    Form
   } from 'semantic-ui-react'
 import * as queries from './graphql/queries'
 import * as mutations from './graphql/mutations'
@@ -83,6 +85,7 @@ const AddBuildDefinition = () => {
 
     return (
         <Segment>
+        <Form>
         <Header as='h3'>Add a new build definition</Header>
         <Input
             type='text'
@@ -132,8 +135,7 @@ const AddBuildDefinition = () => {
             value={printerMainboard}
             onChange={(e) => setPrinterMainboard(e.target.value)}
         /><br/>
-        <Input
-            type='text'
+        <TextArea
             label='Config JSON'
             placeholder='Config JSON'
             name='configurationJSON'
@@ -145,29 +147,123 @@ const AddBuildDefinition = () => {
             content='Create'
             onClick={handleSubmit}
         />
+        </Form>
         </Segment>
         );
     }
   
 const BuildDefinitionDetails = (props) => {
-    const [buildDefinition, setBuildDefinition] = useState([])
+    const [ID, setId] = useState('');
+    const [name, setName] = useState('');
+    const [sourceTree, setSourceTree] = useState('');
+    const [configTree, setConfigTree] = useState('');
+    const [printerManufacturer, setPrinterManufacturer] = useState('')
+    const [printerModel, setPrinterModel] = useState('')
+    const [printerMainboard, setPrinterMainboard] = useState('')
+    const [configurationJSON, setConfigurationJSON] = useState('{}')
     let id = props.match.params.id
   
     useEffect(() => {
       async function fetchData() {
         try {
           const result = await API.graphql(graphqlOperation(queries.getBuildDefinition, {id: id}));
-          setBuildDefinition(result.data.getBuildDefinition)        
+          const buildDefinition = result.data.getBuildDefinition
+          setId(buildDefinition.id)
+          setName(buildDefinition.name)
+          setSourceTree(buildDefinition.sourceTree)
+          setConfigTree(buildDefinition.configTree)
+          setPrinterManufacturer(buildDefinition.printerManufacturer)
+          setPrinterModel(buildDefinition.printerModel)
+          setPrinterMainboard(buildDefinition.printerMainboard)
+          setConfigurationJSON(buildDefinition.configurationJSON)
         } catch (error) {
           console.error(error);
         }
       }
       fetchData();
     }, [])
-  
+
+    const handleSubmit = async(event) => {
+        event.preventDefault();
+        if(name === '' || sourceTree === '' || configTree === '')
+        {
+        alert("All fields have to be filled")
+        return false
+        }
+        let result = await API.graphql(graphqlOperation(mutations.updateBuildDefinition, {input: {
+          id:ID, name, sourceTree, configTree, printerManufacturer, printerModel, printerMainboard, configurationJSON
+        }}));
+        console.log(result);
+        alert("Changes saved")
+    }    
+
     return (
-      <h1>{buildDefinition.name}</h1>
-    )
+        <Segment>
+        <Form>
+        <Header as='h3'>Edit build definition</Header>
+        <Input
+            type='text'
+            label='Build Definition Name'
+            placeholder='Build Definition Name'
+            name='name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}/>
+        <br/>
+        <Input
+            label='Source tree URL'
+            type='text'
+            placeholder='Source tree URL'
+            name='sourceTree'
+            value={sourceTree}
+            onChange={(e) => setSourceTree(e.target.value)}
+        /><br/>
+        <Input
+            type='text'
+            label='Config tree URL'
+            placeholder='Config tree URL'
+            name='configTree'
+            value={configTree}
+            onChange={(e) => setConfigTree(e.target.value)}
+        /><br/>
+
+        <Input
+            type='text'
+            label='Printer manufacturer'
+            placeholder='Printer manufacturer'
+            name='printerManufacturer'
+            value={printerManufacturer}
+            onChange={(e) => setPrinterManufacturer(e.target.value)}
+        /><br/>
+        <Input
+            type='text'
+            label='Printer model'
+            placeholder='Printer model'
+            name='printerModel'
+            value={printerModel}
+            onChange={(e) => setPrinterModel(e.target.value)}
+        /><br/>
+        <Input
+            type='text'
+            label='Printer mainboard type'
+            placeholder='Printer mainboard type'
+            name='printerMainboard'
+            value={printerMainboard}
+            onChange={(e) => setPrinterMainboard(e.target.value)}
+        /><br/>
+        <TextArea
+            label='Config JSON'
+            placeholder='Config JSON'
+            name='configurationJSON'
+            value={configurationJSON}
+            onChange={(e) => setConfigurationJSON(e.target.value)}
+        /><br/>            
+        <Button
+            content='Update'
+            onClick={handleSubmit}
+        />
+        </Form>
+        </Segment>
+        );
   }
 
 export { BuildDefinitionDetails, AddBuildDefinition, BuildDefinitionsList }
