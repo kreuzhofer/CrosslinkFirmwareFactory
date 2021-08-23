6cad38e7936b7cde85b1ff1305024838318921ba
Daniel Kreuzhofer <1763467+kreuzhofer@users.noreply.github.com>
2021-08-23 20:58:00 +0200

    whitespaces

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.render(
 //  <React.StrictMode>
       <>
       <Helmet>
-      <script id="mcjs">{`!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/cab6ca52cf3bc634f70afb049/4a7f11bf418554eac02b1c35f.js");`}</script>
+      <script id="mcjs">{`!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/cab6ca52cf3bc634f70afb049/4a7f11bf418554eac02b1c35f.js");`}</script>      
       </Helmet>
       <MyApp Component={App}/>
       </>
