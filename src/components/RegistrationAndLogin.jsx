7da310b6ffd3b8c68ca341b125ae76153b26f06f
Daniel Kreuzhofer <1763467+kreuzhofer@users.noreply.github.com>
2021-09-07 11:58:22 +0200

    update css package

--- src/components/RegistrationAndLogin.jsx
+++ src/components/RegistrationAndLogin.jsx
@@ -6,7 +6,7 @@ import {
 export class RegistrationAndLogin extends React.Component{
 	render(){
 		return (
-			<div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+			<div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
       <table>
           <tbody>
           <tr>
--- src/index.js
+++ src/index.js
@@ -7,20 +7,19 @@ import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
 import MyApp from './components/amplify-authentication';
 import {Helmet} from "react-helmet";
+import 'semantic-ui-css/semantic.min.css'
 
 Amplify.configure(awsExports);
 
 ReactDOM.render(
-//  <React.StrictMode>
+  <React.StrictMode>
       <>
       <Helmet>
-      <script id="mcjs">{`!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/cab6ca52cf3bc634f70afb049/4a7f11bf418554eac02b1c35f.js");`}</script>      
+        <script id="mcjs">{`!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/cab6ca52cf3bc634f70afb049/4a7f11bf418554eac02b1c35f.js");`}</script>      
       </Helmet>
       <MyApp Component={App}/>
       </>
-//      <PublicApp />
-//  </React.StrictMode>
-
+  </React.StrictMode>
   ,
   document.getElementById('root')
 );
